ec8dc20276adf0a48101e8aecd9975b5e08591a3
Sluttmann <Sluttmann@mail.usf.edu>
2019-09-09 16:45:34 -0800

    Added comments, error message

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * 
+ * coder: Shane Luttmann
+ * 9/9/19
+ * Deliverable 2 - Iterative Statement
+ * 
+ */
+
+using System;
 
 namespace Deliverable_2_Iterative_Statement
 {
@@ -6,34 +14,44 @@ namespace Deliverable_2_Iterative_Statement
     {
         static void Main(string[] args)
         {
+
+
+            // Use of a Try Catch Statement
             try
             {
+                // Prompt for user input
                 Console.Write("Enter a number between 1 and 100: ");
+                // Parse input to int
                 int number = int.Parse(Console.ReadLine());
 
+                // If statement to determin if user input is between 1 and 100
                 if (number > 0 && number <= 100)
                 {
+                    // loopNum used to loop the while loop while keeping number in tact
                     int loopNum = number;
 
+                    // While loop that will loop through what the user has input. While loop will loop till loopNum reaches 0
                     while (loopNum > 0)
                     {
+                        // writeline to display output
                         Console.WriteLine("You have entered: " + number + " This is the current integer value in the loop: " + loopNum);
+                        // decrease loopNum down by one to provent infident loop
                         loopNum--;
                     }
                 }
                 else
                 {
+                    // If user inputs a number greater than 100 or less than 1 this text will display for the user
                     Console.WriteLine("**Enter a number between 1 and 100**");
                 }
 
             }
-            catch
+            // catch statement if any errors occure from above code
+            catch (Exception ex)
             {
-                Console.WriteLine("ERROR!");
+                // Error message
+                Console.WriteLine(ex.Message);
             }
-
-
-
         }
     }
 }
